bda64b06608097d647dc3a8b51ca8e47a65055f2
John M <jwmwalrus@gmail.com>
2020-12-20 22:43:11 -0500

    Add Bus.TimedPopFiltered function

--- bus.go
+++ bus.go
@@ -8,6 +8,7 @@ import "C"
 
 import (
 	"runtime"
+	"time"
 )
 
 type Bus struct {
@@ -31,6 +32,24 @@ func (b *Bus) Pop() (message *Message) {
 func (b *Bus) Pull(messageType MessageType) (message *Message) {
 
 	CGstMessage := C.gst_bus_poll(b.C, C.GstMessageType(messageType), 18446744073709551615)
+	if CGstMessage == nil {
+		return nil
+	}
+
+	message = &Message{
+		C: CGstMessage,
+	}
+
+	runtime.SetFinalizer(message, func(message *Message) {
+		C.gst_message_unref(message.C)
+	})
+
+	return
+}
+
+func (b *Bus) TimedPopFiltered(timeout time.Duration, messageType MessageType) (message *Message) {
+
+	CGstMessage := C.gst_bus_timed_pop_filtered(b.C, C.GstClockTime(uint64(timeout)), C.GstMessageType(messageType))
 	if CGstMessage == nil {
 		return nil
 	}
